afad3a2059e2e94ec056600c40756e23114c5da3
Gholibjon <gholibjonmadiyarov@gmail.com>
2025-07-25 15:29:41 +0500

    *

--- Bee.DateTime/DateTime.cs
+++ Bee.DateTime/DateTime.cs
@@ -1,69 +1,35 @@
 ﻿using System;
-using System.Threading;
 
 namespace Bee.DateTime
 {
     public class DateTime
     {
-        private static Thread t;
-        private static long ts = 0;
-
-        private static System.DateTime dt = System.DateTime.Now;
-
-        public static bool start(long timestamp = 0)
-        {
-            try
-            {
-                if (timestamp < 0 || timestamp == 0)
-                    return false;
-
-                ts = timestamp;
-
-                t = new Thread(start);
-                t.IsBackground = true;
-                t.Start();
-
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public static System.DateTime? getDateTime()
+        public static System.DateTime? getDateTimeFromTimestamp(long timestamp)
         {
             System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dateTime = dateTime.AddSeconds(ts).ToLocalTime();
+            dateTime = dateTime.AddSeconds(timestamp).ToLocalTime();
 
             return dateTime;
         }
 
         public static string getDateTimeString(string format = "yyyy-MM-dd HH:mm:ss")
         {
-            System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            string datetimeFromString = dateTime.AddSeconds(ts).ToLocalTime().ToString(format);
+            var dt = System.DateTime.Now.ToString(format);
 
-            return datetimeFromString;
+            return dt;
+        }
+
+        public static System.DateTime getNow()
+        {
+            var dt = System.DateTime.Now;
+
+            return dt;
         }
 
         public static long? getTimestamp()
         {
-            return ts;
-        }
-
-        private static void start()
-        {
-            while (true)
-            {
-                if (System.DateTime.Now >= dt)
-                {
-                    dt = System.DateTime.Now.AddSeconds(1);
-                    ts++;
-                }
-
-                Thread.Sleep(1);
-            }
+            var ts = (long)(DateTimeOffset.Now - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds;
+            return ts ;
         }
     }
 }
